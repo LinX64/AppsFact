dc8ed9dd6685c750703894ec73d9e13b0996fcee
Mohsen <ash.wxrz@hotmail.com>
2022-10-01 18:04:21 +0300

    Added constants and getTopAlbums

--- app/src/main/java/com/example/appsfactory/data/remote/api/ApiService.kt
+++ app/src/main/java/com/example/appsfactory/data/remote/api/ApiService.kt
@@ -1,7 +1,9 @@
 package com.example.appsfactory.data.remote.api
 
-import com.example.appsfactory.BuildConfig
-import com.example.appsfactory.data.model.ArtistSearchResponse
+import com.example.appsfactory.BuildConfig.API_KEY
+import com.example.appsfactory.data.model.artistList.ArtistSearchResponse
+import com.example.appsfactory.data.model.top_albums.TopAlbumsResponse
+import com.example.appsfactory.data.remote.util.Constants.FORMAT
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,7 +12,14 @@ interface ApiService {
     @GET("?method=artist.search")
     suspend fun artistSearch(
         @Query("artist") artist: String,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY,
-        @Query("format") format: String = "json"
+        @Query("api_key") api_key: String = API_KEY,
+        @Query("format") format: String = FORMAT
     ): ArtistSearchResponse
+
+    @GET("?method=artist.gettopalbums")
+    suspend fun getArtistTopAlbums(
+        @Query("artist") artist: String,
+        @Query("api_key") api_key: String = API_KEY,
+        @Query("format") format: String = FORMAT
+    ): TopAlbumsResponse
 }
---
+++ app/src/main/java/com/example/appsfactory/data/remote/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.appsfactory.data.remote.util
+
+import com.example.appsfactory.BuildConfig
+
+object Constants {
+    const val FORMAT = "json"
+}
