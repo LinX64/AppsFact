6b2c0214901636c0c3a518367e12ac32e1f61c79
Mohsen <ash.wxrz@hotmail.com>
2022-10-02 12:01:22 +0300

    Using viewBinding in MainActivity

--- app/src/main/java/com/example/appsfactory/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/appsfactory/ui/view/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.appsfactory.ui.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.appsfactory.R
+import com.example.appsfactory.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        setSupportActionBar(binding.toolbar)
     }
 }
