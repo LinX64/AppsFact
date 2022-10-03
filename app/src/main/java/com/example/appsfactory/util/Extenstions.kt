ce7c1456b362fa1ad95005ec2ab2c22fe986a4ad
Mohsen <ash.wxrz@hotmail.com>
2022-10-03 12:27:49 +0300

    Removed the internet connection check

--- app/src/main/java/com/example/appsfactory/util/Extenstions.kt
+++ app/src/main/java/com/example/appsfactory/util/Extenstions.kt
@@ -24,24 +24,3 @@ fun View.hideSoftInput() {
         context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
 }
-
-val Context.isConnected: Boolean
-    get() {
-        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
-                val nw = connectivityManager.activeNetwork ?: return false
-                val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
-                when {
-                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                    else  ->  false
-                }
-            }
-            else -> {
-                // Use depreciated methods only on older devices
-                val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-                nwInfo.isConnected
-            }
-        }
-    }
